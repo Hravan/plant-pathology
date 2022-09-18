7e82e9cdae5e1dd805c07ec4984de2536a8aea83
Hravan <krzysztof.krenc@gmail.com>
2022-09-18 22:35:39 +0200

    Cleaned up code.

--- plant_pathology/data.py
+++ plant_pathology/data.py
@@ -1,45 +1,84 @@
+from pathlib import Path
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MultiLabelBinarizer
 import tensorflow as tf
 
+tf.data.experimental.enable_debug_mode()
 
-def load_image(
-    path, image_size, num_channels, interpolation, crop_to_aspect_ratio=False
-):
-    """Load an image from a path and resize it."""
-    img = tf.io.read_file(path)
-    img = tf.image.decode_image(img, channels=num_channels, expand_animations=False)
-    if crop_to_aspect_ratio:
-        img = tf.keras.preprocessing.image.smart_resize(
-            img, image_size, interpolation=interpolation
+
+class DatasetHandler:
+    def __init__(self, data_csv, images_path):
+        df = self._read_csv(data_csv, images_path)
+        images = df["image"].values
+        labels = df["label"].str.split().values
+        self.binarizer = MultiLabelBinarizer()
+        labels_binarized = self.binarizer.fit_transform(labels)
+        self.train_ds, self.val_ds = self._create_datasets(
+            images, labels_binarized
         )
-    else:
-        img = tf.image.resize(img, image_size, method=interpolation)
-    img.set_shape((image_size[0], image_size[1], num_channels))
-    return img
+
+    @staticmethod
+    def _read_csv(data_csv, images_path):
+        df = pd.read_csv(data_csv)
+        df["image"] = df["image"].apply(
+            lambda image_filename: str(Path(images_path) / image_filename)
+        )
+        df = df.rename(columns={"labels": "label"})
+        return df
+
+    @staticmethod
+    def _create_datasets(images, labels):
+        x_train, x_val, y_train, y_val = train_test_split(
+            images,
+            labels,
+            test_size=0.2,
+        )
+        train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+        val_ds = tf.data.Dataset.from_tensor_slices((x_val, y_val))
+        return train_ds, val_ds
 
 
-def create_datasets(images_path, images_csv, used_classes=None, random_state=None):
-    images_df = pd.read_csv(images_csv)
-    if not images_path.endswith("/"):
-        images_path += "/"
-    images_df["image"] = images_path + images_df["image"]
+class ImageReader:
+    def __init__(self, image_size, interpolation, crop_to_aspect_ratio):
+        self.image_size = image_size
+        self.interpolation = interpolation
+        self.crop_to_aspect_ratio = crop_to_aspect_ratio
 
-    label_encoder = LabelEncoder()
+    @staticmethod
+    def load_image(
+        path,
+        image_size,
+    ):
+        """Load an image from a path and resize it."""
+        *_, num_channels = image_size
+        img = tf.io.read_file(path)
+        img = tf.image.decode_image(
+            img, channels=num_channels, expand_animations=False
+        )
+        return img
 
-    if used_classes is not None:
-        images_df = images_df.query("labels in @used_classes")
-    labels = label_encoder.fit_transform(images_df["labels"])
+    @staticmethod
+    def crop(image, image_size, interpolation, crop_to_aspect_ratio=False):
+        height, width, _ = image_size
+        if crop_to_aspect_ratio:
+            image = tf.keras.preprocessing.image.smart_resize(
+                image, image_size, interpolation=interpolation
+            )
+        else:
+            image = tf.image.resize(
+                image, (height, width), method=interpolation
+            )
+        # image.set_shape((image_size[0], image_size[1], num_channels))
+        return image
 
-    x_train, x_val, y_train, y_val = train_test_split(
-        images_df["image"],
-        labels,
-        stratify=labels,
-        test_size=0.2,
-        random_state=random_state,
-    )
-
-    train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-    val_ds = tf.data.Dataset.from_tensor_slices((x_val, y_val))
-    return train_ds, val_ds, label_encoder
+    def __call__(self, image_path):
+        image = self.load_image(image_path, self.image_size)
+        image = self.crop(
+            image,
+            self.image_size,
+            self.interpolation,
+            self.crop_to_aspect_ratio,
+        )
+        image /= 255
+        return image
--- plant_pathology/train_model.py
+++ plant_pathology/train_model.py
@@ -1,4 +1,5 @@
 import argparse
+from functools import partial
 import pathlib
 
 import numpy as np
@@ -8,108 +9,59 @@ from sklearn.utils import compute_class_weight
 import tensorflow as tf
 
 from plant_pathology.confusion_matrix import ConfusionMatrixLogger
-from plant_pathology.data import load_image, create_datasets
+from plant_pathology.data import DatasetHandler, ImageReader
+from plant_pathology import nets
 
 
 def main():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("images_path")
     arg_parser.add_argument("annotations_path")
+    arg_parser.add_argument("batch_size", type=int)
     args = arg_parser.parse_args()
 
-    train_model(args.images_path, args.annotations_path)
+    train_model(args.images_path, args.annotations_path, args.batch_size)
 
 
-def create_model(input_shape, n_outputs):
-    model = tf.keras.models.Sequential(
-        [
-            tf.keras.layers.RandomFlip(input_shape=input_shape),
-            tf.keras.layers.RandomRotation(0.5),
-            tf.keras.layers.Normalization(
-                axis=-1,
-                mean=[0.47261885, 0.62500066, 0.39250168],
-                variance=[[0.02853401, 0.0218361, 0.03131478]],
-            ),
-            tf.keras.layers.Conv2D(32, 11, padding="same", activation="relu"),
-            tf.keras.layers.MaxPooling2D((2, 2)),
-            tf.keras.layers.Conv2D(16, 5, padding="same", activation="relu"),
-            tf.keras.layers.MaxPooling2D((2, 2)),
-            tf.keras.layers.Conv2D(8, 3, padding="same", activation="relu"),
-            tf.keras.layers.MaxPooling2D((2, 2)),
-            tf.keras.layers.Flatten(),
-            tf.keras.layers.Dense(1000, activation="relu"),
-            tf.keras.layers.Dense(100, activation="relu"),
-            tf.keras.layers.Dense(n_outputs),
-        ]
+def train_model(images_path, annotations_csv_path, batch_size):
+    dataset = DatasetHandler(annotations_csv_path, images_path)
+
+    image_reader = ImageReader((200, 300, 3), "bilinear", False)
+
+    train_ds = (
+        dataset.train_ds.shuffle(len(dataset.train_ds))
+        .map(lambda x, y: (image_reader(x), y))
+        .batch(batch_size)
+        .prefetch(tf.data.AUTOTUNE)
     )
+
+    val_ds = dataset.val_ds.map(lambda x, y: (image_reader(x), y)).batch(
+        batch_size
+    )
+
+    model = nets.create_model((200, 300, 3), n_outputs=6)
     model.compile(
-        loss=tf.keras.losses.BinaryCrossentropy(from_logits=True)
-        if n_outputs == 1
-        else tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-        metrics=["accuracy"],
+        loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+        metrics=["categorical_accuracy"],
         optimizer="adam",
     )
-    return model
 
-
-def gen_to_ds(ds_gen):
-    return tf.data.Dataset.from_generator(
-        ds_gen,
-        output_signature=(
-            (
-                tf.TensorSpec(shape=(None, None, 3), dtype=tf.uint8),
-                tf.TensorSpec(shape=tuple(), dtype=tf.uint8),
-            )
-        ),
-    )
-
-
-def preprocess_img(
-    image_path, label, img_shape=(224, 224, 3), crop_to_aspect_ratio=True
-):
-    height, width, n_channels = img_shape
-    img = load_image(
-        image_path,
-        (height, width),
-        n_channels,
-        "bilinear",
-        crop_to_aspect_ratio=crop_to_aspect_ratio,
-    )
-    img = img / 255
-    return img, label
-
-
-def train_model(images_path, annotations_path):
-    used_classes = [
-        "scab",
-        "healthy",
-        "frog_eye_leaf_spot",
-        "rust",
-        "complex",
-        "powdery_mildew",
-    ]
-    input_shape = (224, 224, 3)
-    train_ds, val_ds, label_encoder = create_datasets(
-        images_path, annotations_path, used_classes, random_state=42
-    )
-    train_ds = train_ds.shuffle(18632).map(preprocess_img).batch(256)
-    val_ds = val_ds.map(preprocess_img).batch(256)
-    model = create_model(input_shape, n_outputs=len(used_classes))
-    log_dir = pathlib.Path("logs-6-classes/6-normalization")
+    # Callbacks
+    log_dir = pathlib.Path("logs/1-baseline")
     tb_callback = tf.keras.callbacks.TensorBoard(str(log_dir))
-    cm_callback = tf.keras.callbacks.LambdaCallback(
-        on_epoch_end=ConfusionMatrixLogger(model, val_ds, label_encoder, log_dir)
-    )
-    y_train = [y.numpy() for _, ys in train_ds for y in ys]
-    class_weights = compute_class_weight(
-        "balanced", classes=np.unique(y_train), y=y_train
-    )
+    # cm_callback = tf.keras.callbacks.LambdaCallback(
+    #    on_epoch_end=ConfusionMatrixLogger(
+    #        model, val_ds, label_encoder, log_dir
+    #    )
+    # )
+    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(log_dir / "model")
+    callbacks = [tb_callback, checkpoint_callback]
     model.fit(
         train_ds,
         validation_data=val_ds,
-        epochs=1000,
-        callbacks=[tb_callback, cm_callback],
-        class_weight=dict(enumerate(class_weights)),
+        epochs=300,
+        callbacks=callbacks,
+        # class_weight=dict(enumerate(class_weights)),
     )
 
     pred = model.predict(val_ds)
